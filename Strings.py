17c568b55b8164f0b804658c11f9a6d2604b893f
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-04-24 12:09:01 -0500

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+#Print String 
+print ("Hello, World!")#double qoutes
+print ('\n') #new line
+print ('Hello, World')#single qoutes
+print("""This String runs
+multiple lines!""") #tripple qoute for multi-line
+print ("This string is "+"awesome")#concatenate
