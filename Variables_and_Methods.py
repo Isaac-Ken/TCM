c224164b1f5a7ccabd078169b8bd5a9bee22518d
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-04-24 14:02:54 -0500

    Create Variables_and_Methods.py

---
+++ Variables_and_Methods.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+#Variables and Methods
+qoute = "All is fair in love and war."
+print (qoute)
+print (qoute.upper())#upper case
+print (qoute.lower())#lower case 
+print (qoute.title())#title cae
+print (len(qoute))
+name = "Isaac" #string
+age = 22 #int int(22)
+gpa=3.8 #flaot  float(3.8)
+print (int(age))
+print (int (22.9))
+
+print("My name is " + name +" and I am " + str(age) + " years old.")
+age += 1
+print (age)
+ 
+birthday = 1
+age += birthday 
+print(age)
