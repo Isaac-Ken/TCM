50ed81fa18be62b4ba9fadf1369b97818bc59a3a
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-04-25 12:25:54 -0500

    Create #Relational_and_Boolean_Operators.py

---
+++ #Relational_and_Boolean_Operators.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+#Relational and Boolean operators
+greater_than = 7 > 5
+less_ than = 5 < 7
+greater_than_equal_to =  7 >= 7
+less_than_equal_to =  7 <= 7
+test_and =(7 > 5) and (5 < 7) #True
+test_and2 =(7 > 5) and (5 > 7) #False
+test_or =(7 > 5) or (5 < 7) #True
+test_or =(7 > 5) or (5 > 7) #True
+test_not = not True #False
